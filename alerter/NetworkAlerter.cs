42984b328eb80d3aff0eca273778101ff4e729c6
Anitha-Bhat <anithabhat97@gmail.com>
2022-04-24 22:23:41 +0530

    Create NetworkAlerter.cs

---
+++ alerter/NetworkAlerter.cs
@@ -0,0 +1,20 @@
+namespace AlerterSpace
+{
+  class NetworkAlerter : INetworkAlerter
+  {
+    private static int NETWORK_ALERTSUCCESS = 200;
+    private static int NETWORK_ALERTFAILURE = 500;
+    private static int THRESHOLD_TEMPERATURE = 200;
+
+    public  int networkAlert(float celcius)
+    {
+      Console.WriteLine("ALERT: Temperature is {0} celcius", celcius);
+      if (celcius < THRESHOLD_TEMPERATURE)
+      {
+        return NETWORK_ALERTFAILURE;
+      }
+
+      return NETWORK_ALERTSUCCESS;
+    }
+  }
+}
