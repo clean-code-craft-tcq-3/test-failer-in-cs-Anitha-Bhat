28acc28e68fa50dd91c38fdaf4c0498bc5ea2dae
Anitha-Bhat <anithabhat97@gmail.com>
2022-04-24 22:26:26 +0530

    Update alerter.cs

--- alerter/alerter.cs
+++ alerter/alerter.cs
@@ -1,31 +1,43 @@
-﻿using System;
+using System;
+using System.Diagnostics;
 
-namespace AlerterSpace {
-    class Alerter {
-        static int alertFailureCount = 0;
-        static int networkAlertStub(float celcius) {
-            Console.WriteLine("ALERT: Temperature is {0} celcius", celcius);
-            // Return 200 for ok
-            // Return 500 for not-ok
-            // stub always succeeds and returns 200
-            return 200;
-        }
-        static void alertInCelcius(float farenheit) {
-            float celcius = (farenheit - 32) * 5 / 9;
-            int returnCode = networkAlertStub(celcius);
-            if (returnCode != 200) {
-                // non-ok response is not an error! Issues happen in life!
-                // let us keep a count of failures to report
-                // However, this code doesn't count failures!
-                // Add a test below to catch this bug. Alter the stub above, if needed.
-                alertFailureCount += 0;
-            }
-        }
-        static void Main(string[] args) {
-            alertInCelcius(400.5f);
-            alertInCelcius(303.6f);
-            Console.WriteLine("{0} alerts failed.", alertFailureCount);
-            Console.WriteLine("All is well (maybe!)\n");
-        }
+namespace AlerterSpace
+{
+  
+  class Alerter
+  {
+    static int alertFailureCount = 0;
+
+    static void CountAlertFailure(float farenheit)
+    {
+      INetworkAlerter networkAlerter=new NetworkAlerter();
+      float celcius = (farenheit - 32) * 5 / 9;
+      int returnCode = networkAlerter.networkAlert(celcius);
+      if (returnCode != 200)
+      {
+        // non-ok response is not an error! Issues happen in life!
+        // let us keep a count of failures to report
+        // However, this code doesn't count failures!
+        // Add a test below to catch this bug. Alter the stub above, if needed.
+        alertFailureCount += 0;
+      }
     }
+
+    static void Main(string[] args)
+    {
+      CountAlertFailure(400.5f);
+      Debug.Assert(alertFailureCount == 0);
+
+      CountAlertFailure(303.6f);
+      Debug.Assert(alertFailureCount == 1);
+
+      CountAlertFailure(100.6f);
+
+      Debug.Assert(alertFailureCount == 2);
+
+      Console.WriteLine("{0} alerts failed.", alertFailureCount);
+      Console.WriteLine("All is well (maybe!)\n");
+      Console.ReadKey();
+    }
+  }
 }
