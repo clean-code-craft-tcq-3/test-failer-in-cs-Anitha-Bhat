24dd2882cdaa6860c943f53fc75b55dfe7db7403
Anitha-Bhat <anithabhat97@gmail.com>
2022-05-03 15:20:46 +0530

    Update tshirts.cs

--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -22,11 +22,11 @@ namespace TshirtSpace
     }
     static void Main(string[] args)
     {
-      Debug.Assert(Size(37) == "S");
-      Debug.Assert(Size(38) == "S");
+       Debug.Assert(Size(37) == "S");
+      Debug.Assert(Size(38) == "M");
       Debug.Assert(Size(40) == "M");
       Debug.Assert(Size(42) == "M");
-      Debug.Assert(Size(43) == "L");
+      Debug.Assert(Size(43) == "L")
       Console.WriteLine("All is well (maybe!)");
     }
   }
