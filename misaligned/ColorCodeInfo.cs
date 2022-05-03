51195eb84f5f77e96ac966ac3d948ef308fdce50
Anitha-Bhat <anithabhat97@gmail.com>
2022-05-03 15:08:11 +0530

    Add files via upload

---
+++ misaligned/ColorCodeInfo.cs
@@ -0,0 +1,9 @@
+namespace misaligned
+{
+  public class ColorCodeInfo
+  {
+    public string majorColor { get; set; }
+    public string minorColor { get; set; }
+    public string printFormat { get; set; }
+  }
+}
---
+++ misaligned/MisalignedTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace MisalignedSpace
+{
+  public class MisalignedTest
+  {
+    static void Main(string[] args)
+    {
+      List<ColorCodeInfo> result =Misaligned.printColorMap();
+      Debug.Assert(result.Count == 25);
+      Debug.Assert(result[0].printFormat.Length == result[result.Count - 1].printFormat.Length);
+      Debug.Assert(result[0].majorColor == "White");
+      Debug.Assert(result[0].minorColor == "Blue");
+      Debug.Assert(result[24].majorColor == "Violet");
+      Debug.Assert(result[24].minorColor == "Slate");
+      Console.WriteLine("All is well (maybe!)");
+      Console.ReadKey();
+    }
+  }
+}
