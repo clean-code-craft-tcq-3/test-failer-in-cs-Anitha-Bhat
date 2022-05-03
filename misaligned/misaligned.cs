95df3a50d6e0ae0b80abbdd9aefa8b951a3d4528
Anitha-Bhat <anithabhat97@gmail.com>
2022-05-03 15:06:51 +0530

    Update misaligned.cs

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -1,45 +1,44 @@
 using System;
-using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace MisalignedSpace
 {
-  class Misaligned
+  public class Misaligned
   {
     static string[] majorColors = { "White", "Red", "Black", "Yellow", "Violet" };
     static string[] minorColors = { "Blue", "Orange", "Green", "Brown", "Slate" };
 
-    static int printColorMap()
+    public static List<ColorCodeInfo> printColorMap()
     {
+      List<ColorCodeInfo> colorCodeList = new List<ColorCodeInfo>();
 
-      int i = 0, j = 0;
+      int i,j;
       for (i = 0; i < 5; i++)
       {
         for (j = 0; j < 5; j++)
         {
           int pairNumber = GetPairNumber(i, j);
+          string colorFormatToDisplay =
+            String.Format("{0,-10} | {1,-10} | {2,-10}", pairNumber, majorColors[i], minorColors[j]);
+          colorCodeList.Add(
+            new ColorCodeInfo
+            {
+              majorColor = majorColors[i],
+              minorColor = minorColors[i],
+              printFormat = colorFormatToDisplay
 
-          if (pairNumber < 1 || pairNumber > majorColors.Length * minorColors.Length)
-          {
-            throw new ArgumentOutOfRangeException(
-              string.Format("Argument PairNumber:{0} is outside the allowed range", pairNumber));
-          }
+            });
 
-          Console.WriteLine("{0} | {1} | {2}", pairNumber, majorColors[i], minorColors[i]);
+          Console.WriteLine(colorFormatToDisplay);
         }
       }
-      return i * j;
+      return colorCodeList;
     }
 
     static int GetPairNumber(int x, int y)
     {
-      return x * 5 + y;
+      return x * 5 + y+1;
     }
 
-    static void Main(string[] args)
-    {
-      int result = printColorMap();
-      Debug.Assert(result == 25);
-      Console.WriteLine("All is well (maybe!)");
-    }
   }
 }
