305f54bce60f2fa27186f8edc33c69b595a1c01e
Anitha-Bhat <anithabhat97@gmail.com>
2022-05-08 16:24:14 +0530

    Update MisalignedTest.cs

--- misaligned/MisalignedTest.cs
+++ misaligned/MisalignedTest.cs
@@ -13,6 +13,16 @@ namespace MisalignedSpace
       Debug.Assert(result[0].printFormat.Length == result[result.Count - 1].printFormat.Length);
       Debug.Assert(result[0].majorColor == "White");
       Debug.Assert(result[0].minorColor == "Blue");
+      
+      Debug.Assert(result[11].majorColor == "Red");
+      Debug.Assert(result[11].minorColor == "Slate");
+      
+      Debug.Assert(result[14].majorColor == "Black");
+      Debug.Assert(result[14].minorColor == "Slate");
+      
+      Debug.Assert(result[20].majorColor == "Yellow");
+      Debug.Assert(result[20].minorColor == "Brown");
+      
       Debug.Assert(result[24].majorColor == "Violet");
       Debug.Assert(result[24].minorColor == "Slate");
       Console.WriteLine("All is well (maybe!)");
