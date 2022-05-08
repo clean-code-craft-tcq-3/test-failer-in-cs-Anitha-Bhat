5ed111d01d0187a9a35435adb3ebd0441155843d
Anitha-Bhat <anithabhat97@gmail.com>
2022-05-08 17:45:05 +0530

    Update MisalignedTest.cs

--- misaligned/MisalignedTest.cs
+++ misaligned/MisalignedTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 
@@ -8,25 +8,34 @@ namespace MisalignedSpace
   {
     static void Main(string[] args)
     {
-      List<ColorCodeInfo> result =Misaligned.printColorMap();
+      List<ColorCodeInfo> result = Misaligned.printColorMap();
       Debug.Assert(result.Count == 25);
       Debug.Assert(result[0].printFormat.Length == result[result.Count - 1].printFormat.Length);
       Debug.Assert(result[0].majorColor == "White");
       Debug.Assert(result[0].minorColor == "Blue");
-      
-      Debug.Assert(result[11].majorColor == "Red");
-      Debug.Assert(result[11].minorColor == "Slate");
-      
+
+      Debug.Assert(result[9].majorColor == "Red");
+      Debug.Assert(result[9].minorColor == "Slate");
+
       Debug.Assert(result[14].majorColor == "Black");
       Debug.Assert(result[14].minorColor == "Slate");
-      
-      Debug.Assert(result[20].majorColor == "Yellow");
-      Debug.Assert(result[20].minorColor == "Brown");
-      
+
+      Debug.Assert(result[19].majorColor == "Yellow");
+      Debug.Assert(result[19].minorColor == "Slate");
+
       Debug.Assert(result[24].majorColor == "Violet");
       Debug.Assert(result[24].minorColor == "Slate");
-      Console.WriteLine("All is well (maybe!)");
+      
+      for (int i= 0; i < result.Count; i++)
+      {
+        Debug.Assert(result[i].printFormat.IndexOf('|') == result[result.Count - (i+1)].printFormat.IndexOf('|'));
+        Debug.Assert(result[i].printFormat.LastIndexOf('|') == result[result.Count - (i + 1)].printFormat.LastIndexOf('|'));
+
+      }
      
+
+      Console.WriteLine("All is well (maybe!)");
+    
     }
   }
 }
